d6ea437947816e5bb7005433eb9b64d53c9c44ce
Willian Santos <m16@m16.com.br>
2023-03-11 18:31:35 -0300

    Initial Commit

--- FormattedText/ContentView.swift
+++ FormattedText/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var password = "1234"
+    @State var someText = "initial text"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Hello World")
+                .fontWeight(.medium)
+            SecureField("Enter a password", text: $password)
+                .padding()
+            Text("password entered: \(password)")
+                .italic()
+            TextField("Enter some text", text: $someText)
+                .padding()
+            Text("\(someText)")
+                .font(.largeTitle)
+                .underline()
+            Text("Changing text color and make it bold")
+                .foregroundColor(Color.blue)
+                .bold()
+            Text("Use kerning to change space between lines of text")
+                .kerning(7)
+            Text("Changing baseline offset")
+                .baselineOffset(100)
+            Text("Strikethrough")
+                .strikethrough()
+            Text("This is a multiline text implemented in SwiftUI. The trailing modifier was added to the text. This text also implements multiple modifiers")
+                .background(Color.yellow)
+                .multilineTextAlignment(.trailing)
+                .lineSpacing(10)
+        }
     }
 }
 
@@ -19,3 +45,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
