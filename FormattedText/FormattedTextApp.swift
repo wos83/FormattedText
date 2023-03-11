318a6559c13f582533bd6a5f43da9d8f374fb099
Willian Santos <m16@m16.com.br>
2023-03-11 18:19:26 -0300

    Initial Commit

---
+++ FormattedText/FormattedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextApp.swift
+//  FormattedText
+//
+//  Created by Willian Santos on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
